dced9410565966698bfc2b7767b9b62a73634fdd
ujin-Bang <qkddnwls14@gmail.com>
2022-02-04 23:12:45 +0900

    QR코드 스캐너 작업중

---
+++ app/src/main/java/com/first/app40/MyBarcodeReaderActivity.kt
@@ -0,0 +1,51 @@
+package com.first.app40
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.first.app40.databinding.ActivityMyBarcodeReaderBinding
+import com.journeyapps.barcodescanner.CaptureManager
+
+class MyBarcodeReaderActivity : AppCompatActivity() {
+    private lateinit var capture: CaptureManager
+
+    lateinit var binding:ActivityMyBarcodeReaderBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_my_barcode_reader)
+
+        capture = CaptureManager(this,binding.barcodeScanner)
+        capture.initializeFromIntent(intent, savedInstanceState)
+        capture.decode()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        capture.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        capture.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        capture.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        capture.onSaveInstanceState(outState)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
--- app/src/main/java/com/first/app40/QrcodeScanActivity.kt
+++ app/src/main/java/com/first/app40/QrcodeScanActivity.kt
@@ -1,29 +1,73 @@
 package com.first.app40
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.first.app40.databinding.ActivityQrcodeScanBinding
 import com.google.zxing.integration.android.IntentIntegrator
 
 class QrcodeScanActivity : BaseActivity() {
+
+    lateinit var binding: ActivityQrcodeScanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qrcode_scan)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_qrcode_scan)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
 
+
+
     }
 
     override fun setValues() {
 
-        initQrcodeScanner()
+
+            }
+
+    fun startBarcodeReader(view:View){
+
+        IntentIntegrator(this).initiateScan()
+
     }
-    private fun initQrcodeScanner() {
+
+    fun startBarcodeReaderCustom(view: View){
 
         val integrator = IntentIntegrator(this)
-        integrator.setBeepEnabled
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("QR 코드를 스캔하여 주세요")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(true)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
 
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+             if(result.contents != null) {
+                 Toast.makeText(this, "scanned: ${result.contents}format: ${result.formatName}", Toast.LENGTH_SHORT).show()
+             }
+            else{
+                 Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+             }
+            if(result.barcodeImagePath != null) {
+                val bitmap = BitmapFactory.decodeFile(result.barcodeImagePath)
+                binding.scannedBitmap.setImageBitmap(bitmap)
+            }
+        }
+
+
+
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+
+        }
+    }
+
 }
